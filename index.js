2b48f77e512c8b02144393190f2df1d46594ca05
asanchy <sanchyclips@gmail.com>
2022-01-10 17:46:23 +0200

    Add index.js and it's code

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+let addButton = document.querySelector('#add');
+let listContainer = document.querySelector('.books');
+let bookAuthor = document.querySelector('#author');
+let bookTitle = document.querySelector('#title');
+let list = document.querySelector('#list');
+let goAddBook = document.querySelector('#add-book');
+let contact = document.querySelector('#contact');
+let booksContainer = document.querySelector('.books-container');
+let inputField = document.querySelector('.input-field');
+let contactInfo = document.querySelector('.contact-info');
+
+window.addEventListener('load', () => {
+  list.classList.add('list');
+  if (localStorage.getItem('books') === null) {
+    booksData.updateData([])
+  }
+  displayLibrary.loadBooks()
+});
+
+addButton.addEventListener('click', () => {
+  manipulateBooks.addBook();
+  displayLibrary.loadBooks();
+});
+list.addEventListener('click', () => {
+  navigation.showList();
+});
+goAddBook.addEventListener('click', () => {
+  navigation.showAddBook();
+});
+contact.addEventListener('click', () => {
+  navigation.showContactInfo();
+});
+
+listContainer.addEventListener('click', (event) => {
+  manipulateBooks.removeBook(event);
+  displayLibrary.loadBooks();
+});
+
