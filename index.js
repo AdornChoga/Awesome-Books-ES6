b44ae70f1ec822f079c6f38c07dca956b23c9746
asanchy <sanchyclips@gmail.com>
2022-01-18 21:12:10 +0200

    Add enter key functionality for enter button:

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import displayLibrary from './modules/display-books.js';
 import manipulateBooks from './modules/edit-library.js';
-import navigation from './modules/navigations.js';
+import {navigation, booksContainer} from './modules/navigations.js';
 import booksData from './modules/books-data.js';
 import { DateTime } from './node_modules/luxon/src/luxon.js';
+import {modalContainer, userName, userData} from './modules/username.js';
 
 const addButton = document.querySelector('#add');
 const listContainer = document.querySelector('.books');
@@ -14,11 +15,26 @@ const emptyMessage = document.querySelector('.empty-message');
 const goToAddPage = document.querySelector('.add-message');
 const bookAuthor = document.querySelector('#author');
 const bookTitle = document.querySelector('#title');
+const greet = document.querySelector('#greet')
+const nameInput = document.querySelector('.user')
+
 
 const displayDate = () => {
   const dt = DateTime.now();
   date.innerHTML = dt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 };
+
+function greetUser() {
+  const hour = DateTime.now().hour
+  console.log(hour)
+  if(hour < 12) {
+    greet.innerHTML = "Good Morning" + " " + userData.fetchData()[0].username;
+  } else if(hour >= 12 && hour < 18) {
+    greet.innerHTML = "Good Afternoon" + " " + userData.fetchData()[0].username;
+  } else if(hour >= 18 ) {
+    greet.innerHTML = "Good Evening" + " " + userData.fetchData()[0].username;
+  }
+}
 
 function libraryMessage() {
   if (booksData.fetchData().length === 0) {
@@ -32,14 +48,12 @@ function mouseChange(e) {
   e.style.cursor = 'pointer';
 }
 
-function keyUp(e) {
+function keyUp(e,btn) {
   if (e.keyCode === 13) {
     e.preventDefault();
-    addButton.click();
+    btn.click();
   }
 }
-
-libraryMessage();
 
 setInterval(displayDate, 1000);
 
@@ -48,9 +62,35 @@ window.addEventListener('load', () => {
   if (localStorage.getItem('books') === null) {
     booksData.updateData([]);
   }
+  if(localStorage.getItem('user') === null) {
+    userData.setData([]);
+    userName();
+  }
+  if(localStorage.getItem('user') !== null && userData.fetchData().length > 0) {
+    greetUser()
+  }
   displayLibrary.loadBooks();
   libraryMessage();
 });
+
+modalContainer.addEventListener('click', (event) => {
+  let name = document.querySelector('.user');
+  if (event.target.classList.contains('enter')) {
+    if (name.value !== '') {
+      event.path[3].innerHTML = '';
+      const userdata = {username: name.value}
+      const localData = userData.fetchData();
+      localData.push(userdata);
+      userData.setData(localData);
+      greetUser()
+    } 
+  }
+  if (event.target.classList.contains('close')) {
+    modalContainer.innerHTML = '';
+  }
+})
+
+libraryMessage();
 
 addButton.addEventListener('click', () => {
   manipulateBooks.addBook();
@@ -59,12 +99,19 @@ addButton.addEventListener('click', () => {
 });
 
 bookAuthor.addEventListener('keyup', (event) => {
-  keyUp(event);
+  keyUp(event,addButton);
 });
 
 bookTitle.addEventListener('keyup', (event) => {
-  keyUp(event);
+  keyUp(event,addButton);
 });
+
+modalContainer.addEventListener('keyup', (event) => {
+  if(event.target.classList.contains('user')) {
+    const enterName = document.querySelector('.enter');
+    keyUp(event, enterName);
+  }
+})
 
 list.addEventListener('click', () => {
   navigation.showList();
